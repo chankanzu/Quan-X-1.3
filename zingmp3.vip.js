233624c18d3bbe565c7df6a8d7ce5e95d7bc3d0a
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-11-05 21:18:47 +0700

    Add files via upload

---
+++ zingmp3.vip.js
@@ -0,0 +1,11 @@
+var obj = JSON.parse($response.body);
+var url = $request.url;
+var cons = "getUserInfo";
+if(url.indexOf(cons) != -1)
+{
+obj["data"]["lastVipExpireTime"]= 253378108800;
+obj["data"]["isIapLog"]= true;
+}
+$done({body: JSON.stringify(obj)});
+
+// Descriptions
