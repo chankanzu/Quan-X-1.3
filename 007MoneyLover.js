665411713779010d03acc83917f7d4344f04720f
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-11-10 21:51:50 +0700

    Update 007MoneyLover.js

--- 007MoneyLover.js
+++ 007MoneyLover.js
@@ -2,16 +2,10 @@ var obj = JSON.parse($response.body);
 
 obj= {
 	"status": true,
-	"data": {
-		"expireDate": "2119-11-10T14:34:58.848Z",
-		"firstPurchase": "2019-11-10T14:34:58.848Z",
-		"lastPurchase": "2019-11-10T14:34:58.848Z",
-		"subscribeMarket": "Other",
-		"subscribeProduct": "premium_sub_year_1",
-		"rwLimit": 10,
-		"acceptSync": true,
-		"purchased": true
-	}
+	"purchased": true,
+	"sync": true,
+	"lm": true,
+	"pen": true,
 };
 
 $done({body: JSON.stringify(obj)});
