c65b1f0ebbc94b6dd027db3346dab74db670734c
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-11-10 21:21:53 +0700

    Update 006pibo.js

--- 006pibo.js
+++ 006pibo.js
@@ -6,7 +6,7 @@ obj= {
 	"msg": "\u30ed\u30b0\u30a4\u30f3\u3057\u307e\u3057\u305f",
 	"user_id": 730107,
 	"uuid": "b9900c5f-fc70-4453-964a-65a6c03c54b1",
-	"login_type": "membership",
+	"login_type": "none",
 	"readable": true,
 	"expires": 4086356907000,
 	"status": "welcome",
