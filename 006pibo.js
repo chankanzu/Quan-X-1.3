d57afecea7e68571b370fc1502712464f6a550a5
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-11-10 21:18:05 +0700

    Create 006pibo.js

---
+++ 006pibo.js
@@ -0,0 +1,19 @@
+var obj = JSON.parse($response.body);
+
+obj= {
+	"success": true,
+	"msg_type": "success",
+	"msg": "\u30ed\u30b0\u30a4\u30f3\u3057\u307e\u3057\u305f",
+	"user_id": 730107,
+	"uuid": "b9900c5f-fc70-4453-964a-65a6c03c54b1",
+	"login_type": "membership",
+	"readable": true,
+	"expires": 4086356907000,
+	"status": "welcome",
+	"pay_kind": "",
+	"email": "9b838806e19142706f04e2fe6a74945e"
+};
+
+$done({body: JSON.stringify(obj)});
+
+//
