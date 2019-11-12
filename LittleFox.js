633320f5b68897fb5ea22582fc95138955bbf55d
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-11-12 21:59:37 +0700

    Update LittleFox.js

--- LittleFox.js
+++ LittleFox.js
@@ -27,6 +27,7 @@ obj= {
 		}]
 	},
 	"status": 200
+	"access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJodHRwczpcL1wvYXBpcy5saXR0bGVmb3guY29tXC9hcGlcL3YxXC9hdXRoXC9jaGFuZ2UiLCJpYXQiOjE1NzM1NzA1NzMsImV4cCI6MTU3NjE2MjU3MywibmJmIjoxNTczNTcwNTczLCJqdGkiOiJFazY3ZUhETkdGc0duWmNkIiwic3ViIjoiVTIwMTgxMTExMjM0MDU2MDEwNyIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjciLCJhdXRoX2tleSI6IjU4MjIxMjU1NzcyMDkzMjUiLCJjdXJyZW50X3VzZXJfaWQiOiJVMjAxODExMTIwMDI3NTYwMTEwIn0.vvSQrGeU1q4wWEFTT4CardFK2RN8AAlYqpZ2kVtncK8LBKPruNvPd7EbeAy7iFZ9A2uXKDuznwTAGDsYzei-3g"
   };
 $done({body: JSON.stringify(obj)});
 //
