2de116b03321e1fca411c3bc49f67d42bd15ebbe
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-11-16 00:49:37 +0700

    Update lingokids.vip.js

--- lingokids.vip.js
+++ lingokids.vip.js
@@ -9,7 +9,7 @@ obj["info"]["subscriptions"]= [
         "platform": "apple",
         "duration_value": 1,
         "starts_at": 1572617692,
-        "ends_at": 1972617692,
+        "ends_at": ‭4099821292‬,
         "auto_renew_status": true,
         "plan_id": "lk.ios.s1m.t1m.p15.v1",
         "state": "active_trial"
