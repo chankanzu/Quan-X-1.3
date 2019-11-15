ef48c5b3d27cf1da1fc5d59384bb6f792e37f38c
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-11-16 01:53:20 +0700

    Update lingokids.vip.js

--- lingokids.vip.js
+++ lingokids.vip.js
@@ -21,25 +21,416 @@ obj= {
 			"product": "unlimited",
 			"platform": "apple",
 			"starts_at": 1573059883,
-			"ends_at": 4099821292‬,
+			"ends_at": 1975651883,
 			"plan_id": "lk.ios.s1m.t1m.p15.v1",
 			"duration_unit": "months",
 			"duration_value": 1,
 			"auto_renew_status": true
 		}, {
 			"id": 741222,
-			"status": "paided",
+			"status": "unpaid",
 			"state": "expired_trial",
 			"product": "unlimited",
 			"platform": "apple",
 			"starts_at": 1567004736,
-			"ends_at": 4099821292‬,
+			"ends_at": 1969683136,
 			"plan_id": "lk.ios.s1m.t1m.p15.v1",
 			"duration_unit": "months",
 			"duration_value": 1,
-			"auto_renew_status": true
+			"auto_renew_status": false
 		}],
-				
+		"display_nps": false,
+		"children": [{
+			"id": 10869128,
+			"name": "Jun 3",
+			"age": 5,
+			"defaultLevel": "1",
+			"level": "1",
+			"gender": "male",
+			"language": null,
+			"avatar": "billy",
+			"birthday": 1406851200,
+			"coupons": ["MOUSELK3"],
+			"deleted": false,
+			"progress": {
+				"goals": {
+					"time_spent": {
+						"weekly": 2400
+					}
+				},
+				"activities": {
+					"cursive_r": {
+						"id": "cursive_r",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"regular_c": {
+						"id": "regular_c",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"lk_colors_audiobook": {
+						"id": "lk_colors_audiobook",
+						"status": "unlocked",
+						"completed_times": 1
+					}
+				},
+				"assessments": {
+					"218": {
+						"id": 218,
+						"status": "unlocked"
+					}
+				},
+				"units": {
+					"1": {
+						"id": 1,
+						"status": "unlocked"
+					}
+				},
+				"words": {
+					"415": {
+						"id": 415,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1573840344,
+							"accepted_at": 1573840394
+						}
+					}
+				}
+			},
+			"granted_unit_ids": [17],
+			"welcome_unit_completed_at": 1566109324,
+			"curriculum_unlocked": false
+		}, {
+			"id": 10869127,
+			"name": "Jun 2",
+			"age": 4,
+			"defaultLevel": "1",
+			"level": "1",
+			"gender": "male",
+			"language": null,
+			"avatar": "lisa",
+			"birthday": 1438387200,
+			"coupons": ["MOUSELK2"],
+			"deleted": false,
+			"progress": {
+				"goals": {
+					"time_spent": {
+						"weekly": 2400
+					}
+				},
+				"activities": {
+					"game_memory_pets": {
+						"id": "game_memory_pets",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"lk_song_three_little_kittens": {
+						"id": "lk_song_three_little_kittens",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"lk_song_hickory_dickory_dock": {
+						"id": "lk_song_hickory_dickory_dock",
+						"status": "unlocked",
+						"completed_times": 1
+					}
+				},
+				"assessments": {
+					"218": {
+						"id": 218,
+						"status": "unlocked"
+					}
+				},
+				"units": {
+					"1": {
+						"id": 1,
+						"status": "unlocked"
+					}
+				},
+				"words": {
+					"136": {
+						"id": 136,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1567004927,
+							"accepted_at": 1567059395
+						}
+					}
+				}
+			},
+			"granted_unit_ids": [16],
+			"welcome_unit_completed_at": 1566109300,
+			"curriculum_unlocked": false
+		}, {
+			"id": 10869099,
+			"name": "Jun",
+			"age": 4,
+			"defaultLevel": "2",
+			"level": "2",
+			"gender": "unknown",
+			"language": null,
+			"avatar": "lisa",
+			"birthday": 1441065600,
+			"coupons": ["MOUSELK1", "MOUSELK2", "MOUSELK3"],
+			"deleted": false,
+			"progress": {
+				"goals": {
+					"time_spent": {
+						"weekly": 2400
+					}
+				},
+				"activities": {
+					"regular_f": {
+						"id": "regular_f",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"regular_e": {
+						"id": "regular_e",
+						"status": "completed",
+						"completed_times": 4
+					},
+					"regular_v": {
+						"id": "regular_v",
+						"status": "completed",
+						"completed_times": 3
+					},
+					"regular_y": {
+						"id": "regular_y",
+						"status": "completed",
+						"completed_times": 3
+					},
+					"lk_farmer_inthe_dell": {
+						"id": "lk_farmer_inthe_dell",
+						"status": "completed",
+						"completed_times": 1
+					},
+					"pyramids_game": {
+						"id": "pyramids_game",
+						"status": "unlocked",
+						"completed_times": 2
+					},
+					"oup_body_parts": {
+						"id": "oup_body_parts",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"colors_sticker_game": {
+						"id": "colors_sticker_game",
+						"status": "unlocked",
+						"completed_times": 2
+					},
+					"colors_memory_game": {
+						"id": "colors_memory_game",
+						"status": "unlocked",
+						"completed_times": 2
+					},
+					"coloring_book_butterfly": {
+						"id": "coloring_book_butterfly",
+						"status": "unlocked",
+						"completed_times": 2
+					},
+					"lk_who_took_cookie": {
+						"id": "lk_who_took_cookie",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"mouseme_1_u1": {
+						"id": "mouseme_1_u1",
+						"status": "completed",
+						"completed_times": 3
+					},
+					"mouseme_1_u2": {
+						"id": "mouseme_1_u2",
+						"status": "completed",
+						"completed_times": 1
+					},
+					"mouseme_2_u5": {
+						"id": "mouseme_2_u5",
+						"status": "completed",
+						"completed_times": 3
+					},
+					"mouseme_M2_U2_I_want_to_be_an_artist_Story": {
+						"id": "mouseme_M2_U2_I_want_to_be_an_artist_Story",
+						"status": "completed",
+						"completed_times": 1
+					},
+					"numbers_sticker_game": {
+						"id": "numbers_sticker_game",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"game_sticker_body": {
+						"id": "game_sticker_body",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"draw_hand": {
+						"id": "draw_hand",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"draw_puppet": {
+						"id": "draw_puppet",
+						"status": "unlocked",
+						"completed_times": 2
+					},
+					"draw_wings": {
+						"id": "draw_wings",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"numbers_2": {
+						"id": "numbers_2",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"words_d_doll": {
+						"id": "words_d_doll",
+						"status": "unlocked",
+						"completed_times": 1
+					},
+					"lk_song_green_grass_grew": {
+						"id": "lk_song_green_grass_grew",
+						"status": "completed",
+						"completed_times": 1
+					},
+					"lk_song_the_world_is_coming_alive": {
+						"id": "lk_song_the_world_is_coming_alive",
+						"status": "completed",
+						"completed_times": 1
+					},
+					"game_discovery_toys": {
+						"id": "game_discovery_toys",
+						"status": "unlocked",
+						"completed_times": 2
+					}
+				},
+				"assessments": {
+					"218": {
+						"id": 218,
+						"status": "unlocked"
+					},
+					"230": {
+						"id": 230,
+						"status": "unlocked"
+					},
+					"232": {
+						"id": 232,
+						"status": "unlocked"
+					},
+					"234": {
+						"id": 234,
+						"status": "unlocked"
+					},
+					"294": {
+						"id": 294,
+						"status": "unlocked"
+					}
+				},
+				"units": {
+					"1": {
+						"id": 1,
+						"status": "unlocked"
+					}
+				},
+				"words": {
+					"86": {
+						"id": 86,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1567519689,
+							"accepted_at": 1567519717
+						}
+					},
+					"132": {
+						"id": 132,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1566111303,
+							"accepted_at": 1566111436
+						}
+					},
+					"164": {
+						"id": 164,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1566110496,
+							"accepted_at": 1566110836
+						}
+					},
+					"263": {
+						"id": 263,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1566121423,
+							"accepted_at": 1566121436
+						}
+					},
+					"329": {
+						"id": 329,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1566111000,
+							"accepted_at": 1566111116
+						}
+					},
+					"356": {
+						"id": 356,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1566110663,
+							"accepted_at": 1566110825
+						}
+					},
+					"418": {
+						"id": 418,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1566111153,
+							"accepted_at": 1566111440
+						}
+					},
+					"613": {
+						"id": 613,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1566110539,
+							"accepted_at": 1566110832
+						}
+					},
+					"776": {
+						"id": 776,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1566111089,
+							"accepted_at": 1566111110
+						}
+					},
+					"872": {
+						"id": 872,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1567519702,
+							"accepted_at": 1567519712
+						}
+					},
+					"921": {
+						"id": 921,
+						"sticker": {
+							"status": "accepted",
+							"awarded_at": 1567954893,
+							"accepted_at": 1568246368
+						}
+					}
+				}
+			},
+			"granted_unit_ids": [15, 16, 17],
+			"welcome_unit_completed_at": 1566108796,
+			"curriculum_unlocked": false
+		}],
+		"profile": null
 	},
 	"auth": {
 		"token": "b2c0af05-40a5-4e97-a339-7b71974ecac5"
@@ -48,9 +439,10 @@ obj= {
 		"require_privacy_policy_acceptance": false,
 		"display_newsletter_opt_in": false,
 		"notifications_extended_body": true,
-		"enable_community": true,
-		"age_based_covers": true
+		"enable_community": false,
+		"age_based_covers": false
 	}
+	
       };
 
 $done({body: JSON.stringify(obj)});
