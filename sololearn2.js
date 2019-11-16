6a348aced5cf8957e74cf8fd7293edfa6472135e
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-11-16 21:49:33 +0700

    Create sololearn2.js

---
+++ sololearn2.js
@@ -0,0 +1,602 @@
+var obj = JSON.parse($response.body);
+obj={
+	"profile": {
+		"codes": 0,
+		"posts": 0,
+		"countryCode": "VN",
+		"skills": [{
+			"id": 1024,
+			"name": "JavaScript Tutorial",
+			"languageName": "JavaScript",
+			"level": 1,
+			"xp": 15,
+			"progress": 0.049180327868,
+			"isCompleted": true,
+			"startDate": "2019-11-15T16:07:07.607",
+			"lastProgressDate": "2019-11-15T16:15:11.06"
+		}],
+		"badges": [{
+			"isUnlocked": true,
+			"unlockDate": "2019-11-15T16:07:07.573",
+			"id": 56,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/56.png",
+			"color": "#8BC34A",
+			"title": "Engaged in!",
+			"description": "Complete a lesson",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": "2019-11-10T15:23:47.637",
+			"id": 103,
+			"points": 10,
+			"icon": "https://api.sololearn.com/uploads/Achievements/103.png",
+			"color": "#43A047",
+			"title": "Verified Account",
+			"description": "Verify your account's email address",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 59,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/59.png",
+			"color": "#8BC34A",
+			"title": "Achiever",
+			"description": "Complete a course",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 62,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/62.png",
+			"color": "#094eb6",
+			"title": "Just Getting Started",
+			"description": "Win a challenge",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 58,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/58.png",
+			"color": "#43A047",
+			"title": "Course Master",
+			"description": "Complete 10 courses",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 93,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/93.png",
+			"color": "#42A5F5",
+			"title": "Intern",
+			"description": "Post a code and get an upvote",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 57,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/57.png",
+			"color": "#009688",
+			"title": "Hat Trick",
+			"description": "Complete 3 courses",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 75,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/75.png",
+			"color": "#03A9F4",
+			"title": "Asker",
+			"description": "Post a question and get an upvote",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 63,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/63.png",
+			"color": "#009688",
+			"title": "Master",
+			"description": "Win 50 challenges using one weapon",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 76,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/76.png",
+			"color": "#0288D1",
+			"title": "Good Question",
+			"description": "Get 5 upvotes on your question",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 64,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/64.png",
+			"color": "#00ACC1",
+			"title": "Unstoppable",
+			"description": "Win 100 challenges using one weapon",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 67,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/67.png",
+			"color": "#AD1457",
+			"title": "On Your Way to Fame",
+			"description": "Flawless victory in a challenge",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 65,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/65.png",
+			"color": "#2196F3",
+			"title": "Practice Makes Perfect",
+			"description": "Win 200 challenges using one weapon",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 68,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/68.png",
+			"color": "#6A1B9A",
+			"title": "Challenge Master",
+			"description": "5 flawless victories in a row using one weapon",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 70,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/70.png",
+			"color": "#EF6C00",
+			"title": "Unbeatable!",
+			"description": "Win 10 challenges in a row using one weapon",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 72,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/72.png",
+			"color": "#2E7D32",
+			"title": "Gaining Experience",
+			"description": "Complete 50 challenges using one weapon",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 78,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/78.png",
+			"color": "#3F51B5",
+			"title": "Top Question",
+			"description": "Get 50 upvotes on your question",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 73,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/73.png",
+			"color": "#BF360C",
+			"title": "The Player",
+			"description": "Complete 100 challenges using one weapon",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 84,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/84.png",
+			"color": "#7CB342",
+			"title": "Good Answer",
+			"description": "Get 5 upvotes on your answer",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 74,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/74.png",
+			"color": "#FFC107",
+			"title": "Respect!",
+			"description": "Complete 500 challenges using one weapon",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 66,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/66.png",
+			"color": "#3F51B5",
+			"title": "Guru",
+			"description": "Win 500 challenges using one weapon",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 77,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/77.png",
+			"color": "#01579B",
+			"title": "Great Question",
+			"description": "Get 10 upvotes on your question",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 69,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/69.png",
+			"color": "#4CAF50",
+			"title": "Rising Star",
+			"description": "Win 5 challenges in a row using one weapon",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 80,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/80.png",
+			"color": "#FFB300",
+			"title": "Question Master",
+			"description": "Post 5 questions with at least 5 upvotes each",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 71,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/71.png",
+			"color": "#C62828",
+			"title": "Epic!",
+			"description": "Win 50 challenges in a row using one weapon",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 79,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/79.png",
+			"color": "#283593",
+			"title": "Popular Question",
+			"description": "Get 100 upvotes on your question",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 61,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/61.png",
+			"color": "#9C27B0",
+			"title": "Respected Citizen",
+			"description": "Reply to a comment in a lesson/quiz and get 5 upvotes",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 89,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/89.png",
+			"color": "#00796B",
+			"title": "Illuminator",
+			"description": "Post 50 answers with at least 5 upvotes each",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 90,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/90.png",
+			"color": "#FFCA28",
+			"title": "Self-Learner",
+			"description": "Answer your own question and get 5 upvotes",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 83,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/83.png",
+			"color": "#9CCC65",
+			"title": "Answerer",
+			"description": "Post an answer and get an upvote",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 60,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/60.png",
+			"color": "#673AB7",
+			"title": "Good Citizen",
+			"description": "Leave a comment in a lesson/quiz",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 95,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/95.png",
+			"color": "#1565C0",
+			"title": "Developer",
+			"description": "Get 10 upvotes on your code",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 82,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/82.png",
+			"color": "#E65100",
+			"title": "Question Guru",
+			"description": "Post 50 questions with at least 5 upvotes each",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 91,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/91.png",
+			"color": "#5E35B1",
+			"title": "Contributor",
+			"description": "Leave a comment in a lesson/quiz with 5 upvotes",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 81,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/81.png",
+			"color": "#FF8F00",
+			"title": "Question Ninja",
+			"description": "Post 10 questions with at least 5 upvotes each",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 94,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/94.png",
+			"color": "#1E88E5",
+			"title": "Junior",
+			"description": "Get 5 upvotes on your code",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 88,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/88.png",
+			"color": "#26A69A",
+			"title": "Teacher",
+			"description": "Post 10 answers with at least 5 upvotes each",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 92,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/92.png",
+			"color": "#AB47BC",
+			"title": "Comment Master",
+			"description": "Leave 5 comments in a lesson/quiz with at least 5 upvotes each",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 85,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/85.png",
+			"color": "#558B2F",
+			"title": "Great Answer",
+			"description": "Get 10 upvotes on your answer",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 96,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/96.png",
+			"color": "#1A237E",
+			"title": "Senior Developer",
+			"description": "Get 50 upvotes on your code",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 97,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/97.png",
+			"color": "#AB47BC",
+			"title": "Coder",
+			"description": "Post 5 codes with at least 5 upvotes each",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 86,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/86.png",
+			"color": "#66BB6A",
+			"title": "Top Answer",
+			"description": "Get 50 upvotes on your answer",
+			"type": 0
+		}, {
+			"isUnlocked": true,
+			"unlockDate": null,
+			"id": 99,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/99.png",
+			"color": "#4A148C",
+			"title": "Code Ninja",
+			"description": "Post 50 codes with at least 5 upvotes each",
+			"type": 0
+		}, {
+			"isUnlocked": false,
+			"unlockDate": null,
+			"id": 87,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/87.png",
+			"color": "#4CAF50",
+			"title": "Popular Answer",
+			"description": "Get 100 upvotes on your answer",
+			"type": 0
+		}, {
+			"isUnlocked": false,
+			"unlockDate": null,
+			"id": 98,
+			"points": 0,
+			"icon": "https://api.sololearn.com/uploads/Achievements/98.png",
+			"color": "#8E24AA",
+			"title": "Code Master",
+			"description": "Post 10 codes with at least 5 upvotes each",
+			"type": 0
+		}, {
+			"isUnlocked": false,
+			"unlockDate": null,
+			"id": 101,
+			"points": 2000,
+			"icon": "https://api.sololearn.com/uploads/Achievements/101.png",
+			"color": "#FFCA28",
+			"title": "Quiz Creator",
+			"description": "Create 20 SoloLearn-approved quizzes in the Quiz Factory.",
+			"type": 0
+		}, {
+			"isUnlocked": false,
+			"unlockDate": null,
+			"id": 102,
+			"points": 5000,
+			"icon": "https://api.sololearn.com/uploads/Achievements/102.png",
+			"color": "#EF6C00",
+			"title": "Quiz Creator Master",
+			"description": "Create 100 SoloLearn-approved quizzes in the Quiz Factory.",
+			"type": 0
+		}, {
+			"isUnlocked": false,
+			"unlockDate": null,
+			"id": 104,
+			"points": 1000,
+			"icon": "https://api.sololearn.com/uploads/Achievements/104.png",
+			"color": "#EF6C00",
+			"title": "Moderator",
+			"description": "Awarded to members who have been selected by our moderation team to help moderate community content.",
+			"type": 0
+		}, {
+			"isUnlocked": false,
+			"unlockDate": null,
+			"id": 105,
+			"points": 5000,
+			"icon": "https://api.sololearn.com/uploads/Achievements/105.png",
+			"color": "#FBC02D",
+			"title": "Gold Moderator",
+			"description": "Awarded to select Moderators who show expertise in moderation and are ready for the next level of responsibilities.",
+			"type": 0
+		}, {
+			"isUnlocked": false,
+			"unlockDate": null,
+			"id": 112,
+			"points": 10000,
+			"icon": "https://api.sololearn.com/uploads/Achievements/112.png",
+			"color": "#0277BD",
+			"title": "Platinum Moderator",
+			"description": "Awarded to experienced moderators who are ready to recruit and train new moderators and curate community content.",
+			"type": 0
+		}, {
+			"isUnlocked": false,
+			"unlockDate": null,
+			"id": 107,
+			"points": 1000,
+			"icon": "https://api.sololearn.com/uploads/Achievements/107.png",
+			"color": "#0277BD",
+			"title": "Quiz Reviewer",
+			"description": "Exclusive badge, handed out to community members who moderate user-generated quizzes.",
+			"type": 0
+		}, {
+			"isUnlocked": false,
+			"unlockDate": null,
+			"id": 108,
+			"points": 5000,
+			"icon": "https://api.sololearn.com/uploads/Achievements/108.png",
+			"color": "#FBC02D",
+			"title": "Gold Quiz Reviewer",
+			"description": "Exclusive badge, handed out to experienced Quiz Reviewers who show expertise in moderation of user-generated quizzes.",
+			"type": 0
+		}, {
+			"isUnlocked": false,
+			"unlockDate": null,
+			"id": 110,
+			"points": 3000,
+			"icon": "https://api.sololearn.com/uploads/Achievements/110.png",
+			"color": "#03A9F4",
+			"title": "Creator",
+			"description": "Create 10 SoloLearn-approved lessons in the Lesson Factory.",
+			"type": 0
+		}, {
+			"isUnlocked": false,
+			"unlockDate": null,
+			"id": 111,
+			"points": 10000,
+			"icon": "https://api.sololearn.com/uploads/Achievements/111.png",
+			"color": "#0277BD",
+			"title": "Creator Master",
+			"description": "Create 50 SoloLearn-approved lessons in the Lesson Factory.",
+			"type": 0
+		}],
+		"xpHistory": [],
+		"contestHistory": [],
+		"skillRanks": {
+			"learner": 0.0037831021436923076,
+			"challenger": 0.0,
+			"contributor": 0.0,
+			"coder": 0.0,
+			"influencer": 0.0
+		},
+		"rank": {
+			"userID": 16386460,
+			"countryCode": "VN",
+			"monthlyTotalRank": null,
+			"monthlyTotalPercent": 26.768964327021479,
+			"monthlyCountryRank": 1328,
+			"monthlyCountryPercent": 27.096510916139565,
+			"weeklyTotalRank": 82920,
+			"weeklyTotalPercent": 23.131900933421857,
+			"weeklyCountryRank": 566,
+			"weeklyCountryPercent": 22.831787010891489
+		},
+		"contestStats": [],
+		"challengeSkills": [{
+			"id": 1024,
+			"name": "JavaScript Tutorial",
+			"languageName": "JavaScript"
+		}],
+		"followers": 0,
+		"following": 1,
+		"isFollowing": false,
+		"id": 16386460,
+		"email": "svcntnk42a1@gmail.com",
+		"name": "Bố Jun",
+		"hasAvatar": true,
+		"avatarUrl": "https://www.sololearn.com/avatars/f86197b7-e05f-41b5-8055-e2d1fbffa196.jpg",
+		"accessLevel": 0,
+		"badge": "pro",
+		"level": 1,
+		"xp": 45,
+		"isPro": true
+	}
+};
+
+$done({body: JSON.stringify(obj)});
