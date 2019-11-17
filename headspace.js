31e8b9823bfa8884f2b1a48ae9225d6a4f5b98c7
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-11-17 23:51:42 +0700

    Update headspace.js

--- headspace.js
+++ headspace.js
@@ -16,13 +16,13 @@ obj= {
 				"currency": "EUR",
 				"symbol": "%E2%82%AC"
 			}, {
-				"price": 7.95,
-				"currency": "GBP",
-				"symbol": "%C2%A3"
-			}, {
 				"price": 12.95,
 				"currency": "USD",
 				"symbol": "%24"
+			}, {
+				"price": 7.95,
+				"currency": "GBP",
+				"symbol": "%C2%A3"
 			}],
 			"subscriptionId": "HSSUBS_B2CSUBSCRIBER"
 		}
@@ -34,8 +34,8 @@ obj= {
 			"name": "Annual (14 day trial)",
 			"description": "An iOS monthly subscription with a 14 day free trial",
 			"code": "IOSIAP_FT_14D_ANNUAL",
-			"numMonths": 36,
-			"isGift": false,
+			"numMonths": 1000,
+			"isGift": true,
 			"voucherId": "HSVCHR_A694L22PN4LCU3FJ",
 			"platform": "iap"
 		}
@@ -47,19 +47,19 @@ obj= {
 			"id": "HSUSUB_E4WLBJPAPZI8YY81",
 			"userSubscriptionId": "HSUSUB_E4WLBJPAPZI8YY81",
 			"startDate": "2019-11-17T15:45:32.0+0000",
-			"endDate": "2220-11-17T15:45:32.0+0000",
+			"endDate": "2020-11-17T15:45:32.0+0000",
 			"status": "ACTIVE",
 			"paymentSchedule": "EVERGREEN",
 			"nextRenewalDate": "2220-11-17T15:45:32.0+0000",
 			"inFinalPeriod": false,
 			"inLast30Days": true,
-			"isLifetime": true,
+			"isLifetime": false,
 			"cancelled": false,
 			"freeTrialEndDate": "2119-12-01T15:45:32.0+0000",
 			"iapOriginalTransactionId": "590000448468341",
 			"subscriptionDetails": {
 				"platform": "APPLE",
-				"term": 36,
+				"term": 12,
 				"version": "V2",
 				"planAffiliation": null,
 				"businessModel": "B2C",
@@ -82,7 +82,6 @@ obj= {
 			}
 		}
 	}
-	
 };
 $done({body: JSON.stringify(obj)});
 
