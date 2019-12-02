feee827700d34f3451ebfa2acb149c8cc1128ad0
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-12-02 20:19:26 +0700

    Create Trial.Moneylover.js

---
+++ Trial.Moneylover.js
@@ -0,0 +1,133 @@
+var obj = JSON.parse($response.body);
+obj={
+	"t": 1550131363,
+	"data": [{
+		"metadata": {
+			"platform": ["ios", "windows"],
+			"discount": 0,
+			"type": "linked_wallet"
+		},
+		"product_id": "finsify_sub_year_1",
+		"name": "Linked Wallet Sub 01-Year (iOS/Windows)",
+		"expire_unit": "years",
+		"expire_value": 1,
+		"price_vn": 149000,
+		"price_gl": 19.99,
+		"alias": ["finsify_sub_year_1", "finsify_sub_year_1_plus"],
+		"has_trial": false
+	}, {
+		"metadata": {
+			"discount": 0,
+			"type": "linked_wallet",
+			"platform": ["ios", "windows"]
+		},
+		"product_id": "finsify_sub_month_6",
+		"name": "Linked Wallet Sub 06 Months (iOS/Windows)",
+		"expire_unit": "months",
+		"expire_value": 6,
+		"price_vn": 99000,
+		"price_gl": 9.99,
+		"alias": ["finsify_sub_month_6", "finsify_sub_month_6_plus"],
+		"has_trial": false
+	}, {
+		"metadata": {
+			"type": "linked_wallet",
+			"discount": 0,
+			"platform": ["ios", "windows"]
+		},
+		"product_id": "finsify_sub_month_1",
+		"expire_unit": "months",
+		"expire_value": 1,
+		"price_vn": 19000,
+		"price_gl": 2.99,
+		"name": "Linked Wallet Sub 01-Month (iOS/Windows)",
+		"alias": ["finsify_sub_month_1", "finsify_sub_month_1_plus"],
+		"has_trial": false
+	}, {
+		"metadata": {
+			"platform": ["android"],
+			"type": "linked_wallet",
+			"discount": 0
+		},
+		"product_id": "finsify_sub_month_1_a",
+		"name": "Linked Wallet, 01M, Android",
+		"expire_unit": "months",
+		"expire_value": 1,
+		"price_vn": 39000,
+		"price_gl": 1.99,
+		"alias": ["finsify_sub_month_1_a"],
+		"has_trial": true
+	}, {
+		"metadata": {
+			"platform": ["android"],
+			"type": "linked_wallet",
+			"discount": 0
+		},
+		"product_id": "finsify_sub_month_6_a",
+		"name": "Linked Wallet, 06M, Android",
+		"expire_unit": "months",
+		"expire_value": 6,
+		"price_vn": 149000,
+		"price_gl": 14.99,
+		"alias": ["finsify_sub_month_6_a"],
+		"has_trial": true
+	}, {
+		"metadata": {
+			"discount": 0,
+			"type": "linked_wallet",
+			"platform": ["android"]
+		},
+		"product_id": "finsify_sub_year_1_a",
+		"name": "Linked Wallet, 12M, Android",
+		"expire_unit": "years",
+		"expire_value": 1,
+		"price_vn": 199000,
+		"price_gl": 19.99,
+		"alias": ["finsify_sub_year_1_a"],
+		"has_trial": true
+	}, {
+		"metadata": {
+			"discount": 0,
+			"type": "premium",
+			"platform": ["android"]
+		},
+		"product_id": "premium_sub_year_1_a5",
+		"name": "Premium Plan: 01 year",
+		"expire_unit": "years",
+		"expire_value": 1,
+		"price_vn": 444000,
+		"price_gl": 19.49,
+		"alias": ["premium_sub_year_1_a5"],
+		"has_trial": false
+	}, {
+		"metadata": {
+			"platform": ["android"],
+			"type": "premium",
+			"discount": 0
+		},
+		"product_id": "premium_sub_month_1_a5",
+		"name": "Premium Plan: 01 month",
+		"expire_unit": "months",
+		"expire_value": 1,
+		"price_vn": 69000,
+		"price_gl": 2.99,
+		"alias": ["premium_sub_month_1_a5"],
+		"has_trial": false
+	}, {
+		"metadata": {
+			"platform": ["android"],
+			"type": "premium",
+			"discount": 0
+		},
+		"product_id": "premium_sub_month_6_a5",
+		"expire_unit": "months",
+		"expire_value": 6,
+		"name": "Premium Plan: 06 month",
+		"price_gl": 14.49,
+		"price_vn": 329000,
+		"alias": ["premium_sub_month_6_a5"],
+		"has_trial": false
+	}]
+  };
+
+$done({body: JSON.stringify(obj)});
