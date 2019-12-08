cd56857b404131bf2ec8097e037e2f054a89a6e4
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-12-08 22:07:55 +0700

    Create 4english3.js

---
+++ 4english3.js
@@ -0,0 +1,19 @@
+{
+	"uses_provisional_auth": true,
+	"outcomes": {
+		"direct": {
+			"enabled": true
+		},
+		"indirect": {
+			"notification_attribution": {
+				"minutes_since_displayed": 600000,
+				"limit": 10000
+			},
+			"enabled": true
+		},
+		"unattributed": {
+			"enabled": true
+		}
+	},
+	"receive_receipts_enable": true
+}
