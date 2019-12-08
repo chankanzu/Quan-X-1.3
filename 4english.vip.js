cf3e3d985f6ea5691fac0baa773dc90c17811b1d
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-12-08 17:59:35 +0700

    Create 4english.vip.js

---
+++ 4english.vip.js
@@ -0,0 +1,16 @@
+var obj = JSON.parse($response.body);
+
+obj= {
+	"code": 0,
+	"data": {
+		"userId": "2fwiWLaVkvg3IxakMqhn6AH8DkA3",
+		"shortUserId": "740187",
+		"email": "svcntnk42a1@gmail.com",
+		"displayName": "Khánh Dư Trần",
+		"providerData": "Firebase",
+		"expiredTime": 4100823704000,
+		"showOxford": true
+};
+$done({body: JSON.stringify(obj)});
+
+// Descriptions
