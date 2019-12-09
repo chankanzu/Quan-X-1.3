20524d7e625aaa748abcabed0122d0314433e0cd
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-12-09 21:46:14 +0700

    Update lumosity3.js

--- lumosity3.js
+++ lumosity3.js
@@ -1,5 +1,7 @@
 //https://www.lumosity.com/api/v2/insights/game_result_stats?end_date=2019-12-09
-{
+var obj = JSON.parse($response.body);
+
+obj= {
 	"data": [{
 		"type": "game_result_stat",
 		"id": "2019-12-09",
@@ -9,6 +11,10 @@
 	}],
 	"meta": {
 		"start_date": "2019-11-09",
-		"end_date": "2019-12-09"
+		"end_date": "2119-12-09"
 	}
-}
+};
+
+$done({body: JSON.stringify(obj)});
+
+//
