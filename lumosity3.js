c23686f5d54a5d411ec1fcf574d0342363c7a8cc
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-12-09 21:45:06 +0700

    Create lumosity3.js

---
+++ lumosity3.js
@@ -0,0 +1,14 @@
+//https://www.lumosity.com/api/v2/insights/game_result_stats?end_date=2019-12-09
+{
+	"data": [{
+		"type": "game_result_stat",
+		"id": "2019-12-09",
+		"attributes": {
+			"gameplays": 3
+		}
+	}],
+	"meta": {
+		"start_date": "2019-11-09",
+		"end_date": "2019-12-09"
+	}
+}
