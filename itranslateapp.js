527b171674d3dd65bf70e3dd9452910a97af1ace
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-12-15 10:28:23 +0700

    Create itranslateapp.js

---
+++ itranslateapp.js
@@ -0,0 +1,24 @@
+
+var obj = JSON.parse($response.body);
+
+obj= {
+	"licenses": [{
+		"is_trial_period": true,
+		"product_id": "com.itranslate.pro.monthly",
+		"original_transaction_id": "590000457497610",
+		"expires_date_ms": 1576985099000,
+		"bundle_id": "com.outerspaceapps.itranslate",
+		"transaction_id": "590000457497610"
+	}, {
+		"is_trial_period": true,
+		"product_id": "com.itranslate.pro.monthly",
+		"original_transaction_id": "590000457497610",
+		"expires_date_ms": 1576985099000,
+		"bundle_id": "com.outerspaceapps.itranslate",
+		"transaction_id": "590000457497610"
+	}]
+};
+
+$done({body: JSON.stringify(obj)});
+
+//
