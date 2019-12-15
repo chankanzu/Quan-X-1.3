d1f76bc3ad248710827397d8e67d04fb8bcff168
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-12-15 10:29:55 +0700

    Update itranslateapp.js

--- itranslateapp.js
+++ itranslateapp.js
@@ -6,14 +6,14 @@ obj= {
 		"is_trial_period": true,
 		"product_id": "com.itranslate.pro.monthly",
 		"original_transaction_id": "590000457497610",
-		"expires_date_ms": 1576985099000,
+		"expires_date_ms": 4081386904000,
 		"bundle_id": "com.outerspaceapps.itranslate",
 		"transaction_id": "590000457497610"
 	}, {
 		"is_trial_period": true,
 		"product_id": "com.itranslate.pro.monthly",
 		"original_transaction_id": "590000457497610",
-		"expires_date_ms": 1576985099000,
+		"expires_date_ms": 4081386904000,
 		"bundle_id": "com.outerspaceapps.itranslate",
 		"transaction_id": "590000457497610"
 	}]
