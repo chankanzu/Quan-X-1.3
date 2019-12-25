4fe4904236486239477fd32d57a44dacac5a7616
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2019-12-26 01:57:38 +0700

    Update Singbot.js

--- Singbot.js
+++ Singbot.js
@@ -7,14 +7,14 @@ obj= {
  "request_date": "2019-12-25T18:44:43Z",
  "request_date_ms": 1577299483002,
  "subscriber": {
-  "entitlements": {},
+  "entitlements":"singbot.annual_subscription",
   "first_seen": "2019-12-25T18:16:44Z",
   "non_subscriptions": {},
   "original_app_user_id": "505C9299-48B3-49EA-94CA-04D683A22703",
   "original_application_version": null,
-  "original_purchase_date": null,
+  "original_purchase_date": 1577299483002,
   "other_purchases": {},
-  "subscriptions": {}
+  "subscriptions": 1977299483002
  }
 };
 
