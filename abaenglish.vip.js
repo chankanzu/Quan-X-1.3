dba51124f0b367357ef8d7ef24cb96646c483838
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-01-04 20:53:53 +0700

    Update abaenglish.vip.js

--- abaenglish.vip.js
+++ abaenglish.vip.js
@@ -1,12 +1,28 @@
 var obj = JSON.parse($response.body);
-
-obj= {
-  "status": "OK",
-  "userLevel": "6",
-  "expirationDate": "06-08-2019",
-  "idPeriod": "360",
-  "token": "e23d5c91f815744271d6dd1aa6a47f29",
-  "userType": "2"
+obj={
+	"request_date": "2020-01-04T13:45:50Z",
+	"request_date_ms": 1578145550873,
+	"subscriber": {
+		"entitlements": {},
+		"first_seen": "2020-01-03T15:21:58Z",
+		"non_subscriptions": {},
+		"original_app_user_id": "22683069",
+		"original_application_version": "475",
+		"original_purchase_date": "2019-11-10T14:07:55Z",
+		"other_purchases": {},
+		"subscriptions": {
+			"12m_freetrial_2018": {
+				"billing_issues_detected_at": null,
+				"expires_date": "2120-01-10T15:35:13Z",
+				"is_sandbox": false,
+				"original_purchase_date": "2020-01-03T15:35:13Z",
+				"period_type": "trial",
+				"purchase_date": "2020-01-03T15:35:13Z",
+				"store": "app_store",
+				"unsubscribe_detected_at": null
+		}
+	}
+}
 };
 
 $done({body: JSON.stringify(obj)});
