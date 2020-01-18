4217aeed005d931efcb9f5cdfbecd0ce6b747b3d
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-01-19 00:04:49 +0700

    Update speendee1.js

--- speendee1.js
+++ speendee1.js
@@ -2,5 +2,5 @@ var body = $response.body;
 let obj = JSON.parse(body);
 obj.result["premium_expiration"] = "2220-01-17 18:47:48";
 obj.result["type"] ="premium",
-obj.result["expiration"] ="2220-01-24"",  
+obj.result["expiration"] = "2220-01-18 16:56:22",  
 $done({body:JSON.stringify(obj)});
