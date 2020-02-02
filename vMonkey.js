4f1fff10a73a8b2d026e4d6fc4c4a5e6ec3f8cf8
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-02-02 21:50:35 +0700

    Delete vMonkey.js

--- vMonkey.js
+++
@@ -1,7 +0,0 @@
-let obj = JSON.parse($response.body)
-obj["time_expire"] = 1978495769;
-obj["pay_inapp"] = true;
-obj["is_active"] = true;
-obj["users_id"] = 5563990;
-obj["is_free"] = false;
-$done({body:JSON.stringify(obj)})
