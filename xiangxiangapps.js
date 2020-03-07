07f915b7e1cfdd10c0e1a8c543e84570dfda1f5e
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-03-07 21:48:11 +0700

    Create xiangxiangapps.js

---
+++ xiangxiangapps.js
@@ -0,0 +1,7 @@
+let obj = JSON.parse($response.body);
+  obj["data"]["dayrows"]["limit_free"] = 0;
+  obj["data"]["dayrows"]["isvip"] = 0;
+  obj["data"]["dayrows"]["islimit"] = 0;
+  obj["data"]["dayrows"]["islimitv3"] = 0;
+  obj["data"]["dayrows"]["exclusive"] = 0;
+$done({body: JSON.stringify(obj)});
