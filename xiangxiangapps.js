f1055bf3713b6edbdf81a7f9bb292acf5d4287bd
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-03-07 22:16:08 +0700

    Update xiangxiangapps.js

--- xiangxiangapps.js
+++ xiangxiangapps.js
@@ -1,7 +1,40 @@
-let obj = JSON.parse($response.body);
-  obj["data"]["vodrow"]["limit_free"] = 100;
-  obj["data"]["vodrow"]["isvip"] = 1;
-  obj["data"]["vodrow"]["islimit"] = 0;
-  obj["data"]["vodrow"]["islimitv3"] = 0;
-  obj["data"]["vodrow"]["exclusive"] = 0;
+var obj = JSON.parse($response.body);
+obj={
+	"retcode": 1000,
+	"errmsg": "",
+	"data": {
+		"user": {
+			"uid": 0,
+			"uniqkey": "0",
+			"username": null,
+			"nickname": null,
+			"mobi": null,
+			"email": null,
+			"sysgid": null,
+			"gid": null,
+			"gids": null,
+			"gicon": "",
+			"isvip": 1,
+			"regtime": "1970-01-01 08:00:00",
+			"gender": null,
+			"avatar": null,
+			"avatar_url": "https:\/\/pic.1024av.cc\/sysavatar\/noavatar.png",
+			"newmsg": null,
+			"goldcoin": 100000,
+			"duetime": "",
+			"dueday": ""
+		},
+		"uinfo": {
+			"goldcoin": "10000",
+			"play_daily_remainders": 1000,
+			"down_daily_remainders": 1000,
+			"curr_group": null,
+			"next_group": null,
+			"next_upgrade_need": 0,
+			"minivod_play_daily_remainders": 200,
+			"minivod_down_daily_remainders": 300
+		},
+		"signed": 1
+	}
+};
 $done({body: JSON.stringify(obj)});
