6a4b9fc9f13d81ed1705e88c18ebc873cb6bfc25
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-03-07 21:51:00 +0700

    Update xiangxiangapps.js

--- xiangxiangapps.js
+++ xiangxiangapps.js
@@ -1,7 +1,7 @@
 let obj = JSON.parse($response.body);
-  obj["data"]["dayrows"]["limit_free"] = 0;
-  obj["data"]["dayrows"]["isvip"] = 0;
-  obj["data"]["dayrows"]["islimit"] = 0;
-  obj["data"]["dayrows"]["islimitv3"] = 0;
-  obj["data"]["dayrows"]["exclusive"] = 0;
+  obj["data"]["vodrow"]["limit_free"] = 100;
+  obj["data"]["vodrow"]["isvip"] = 1;
+  obj["data"]["vodrow"]["islimit"] = 0;
+  obj["data"]["vodrow"]["islimitv3"] = 0;
+  obj["data"]["vodrow"]["exclusive"] = 0;
 $done({body: JSON.stringify(obj)});
