ba3e69d05d0f9e615292c8c37ead21644050eeae
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-07-05 22:10:57 +0700

    Update shapr3d.js

--- shapr3d.js
+++ shapr3d.js
@@ -1,5 +1,5 @@
 var obj = JSON.parse($response.body);
-obj["subscriptionType"]= "edu";
+obj["subscriptionType"]= "VIP";
 obj["subscriptionExpires"]= "2099-05-22T09:14:44.394Z";
 obj["hasEmail"]= true;
 $done({body: JSON.stringify(obj)});
