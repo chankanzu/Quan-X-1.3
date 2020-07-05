6bb580acaa5059175da7e3b97c5d0f8b6c1d0721
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-07-05 22:48:44 +0700

    Update 4english3.js

--- 4english3.js
+++ 4english3.js
@@ -1,6 +1,7 @@
 let obj = JSON.parse($response.body)
 obj.data.expiredTime = "4086356907000";
 obj.data.pro = true;
+obj.data.showOxford = true;
 obj.data.shortUserId = "000286";
 $done({body:JSON.stringify(obj)})
 //https://vocabinnews.com/v1/users/me/pro
