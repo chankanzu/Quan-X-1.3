6039f4195cb13ca2ba139b3b1f2f931593772e11
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-08-10 15:50:43 +0700

    Update ejoy.js

--- ejoy.js
+++ ejoy.js
@@ -1,6 +1,6 @@
 let obj = JSON.parse($response.body);
 obj["data"]["checkSub"]["pro"]=true;
-obj["data"]["checkSub"]["expire"]=1597916480079;
+obj["data"]["checkSub"]["expire"]=1997916480079;
 obj["data"]["getCoinBalance"]=199999;
 $done({body: JSON.stringify(obj)});
 
