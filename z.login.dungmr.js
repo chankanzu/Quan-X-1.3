c91104fd84b1322546ebcfddbcd0eeaba5ca8e17
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-09-09 02:22:05 +0700

    Update z.login.dungmr.js

--- z.login.dungmr.js
+++ z.login.dungmr.js
@@ -4,7 +4,7 @@ obj= {
 	"id": 7,
 	"email": "dungmori@gmail.com",
 	"phone_number": null,
-	"name": "dungmori",
+	"name": "D\u0169ng Mori\",
 	"phone": "0983639675",
 	"gender": "Nam",
 	"avatar": "1524024189_86415_8f14e4_a55c90.jpg",
