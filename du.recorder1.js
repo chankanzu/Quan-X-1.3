7e5868a694c534c96e61a07d4b97d5a529cce902
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-09-12 22:49:52 +0700

    Update du.recorder1.js

--- du.recorder1.js
+++ du.recorder1.js
@@ -1,15 +1,4 @@
 //http://bbdd2101-default-recorders-4916-415735771.us-west-2.elb.amazonaws.com/pay/checkAppleSubscribeReceipt*
-var obj = JSON.parse($response.body);
-
-obj= {
-	"code": 200,
-	"result": [{
-		"expiresDateMs": 1600480559000,
-		"productId": "recorder_1_year"
-	}],
-	"serverTime": 1599925138672
-};
-
-$done({body: JSON.stringify(obj)});
-
-//
+let obj = JSON.parse($response.body)
+obj.result.expiresDateMs = 1900480559000;
+$done({body:JSON.stringify(obj)})
