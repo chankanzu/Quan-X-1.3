d802eb0ee129d7010b07bc27137456ab65e35362
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-09-12 22:52:15 +0700

    Update du.recorder1.js

--- du.recorder1.js
+++ du.recorder1.js
@@ -1,4 +1,5 @@
 //http://bbdd2101-default-recorders-4916-415735771.us-west-2.elb.amazonaws.com/pay/checkAppleSubscribeReceipt*
 let obj = JSON.parse($response.body)
 obj.result.expiresDateMs = 1900480559000;
+obj.result.productId = "recorder_1_year";
 $done({body:JSON.stringify(obj)})
