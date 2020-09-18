720929a5742d62b4f5d8a6297630b220c0e73317
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-09-18 22:42:09 +0700

    Create alokid.js

---
+++ alokid.js
@@ -0,0 +1,52 @@
+var obj = JSON.parse($response.body);
+obj= {
+	"success": 1,
+	"data": {
+		"UserName": "alokiddyvip1",
+		"Password": "E10ADC3949BA59ABBE56E057F20F883E",
+		"FullName": "alokiddyvip1",
+		"Email": "hoanhtk5@gmail.com",
+		"LastVisited": "\/Date(1600443324710)\/",
+		"Logins": 8899,
+		"Status": 1,
+		"Phone": "0987654321",
+		"Rate": 0,
+		"Imagefile": "/Uploads/member/avatar/30-8DD787B72D3B3114D38AABEA635EDC6E.jpg",
+		"giaovien": 0,
+		"Facebook_uid": null,
+		"Google_uid": null,
+		"EnglishLevel": 3,
+		"DateOfBirth": "\/Date(1207674000000)\/",
+		"IsHideDOB": 1,
+		"Gender": 1,
+		"DateCreated": "\/Date(1438230177000)\/",
+		"Address": "hanboi",
+		"GenderName": "Nam",
+		"CountLike": 0,
+		"MemberCode": null,
+		"IsVip": 1,
+		"TotalMoney": 1655000,
+		"IpLocation": null,
+		"KhuVuc": null,
+		"LinkFace": null,
+		"QuanLy": 0,
+		"HocSinh": 0,
+		"TruongHocId": 0,
+		"MemberKeyActivity": null,
+		"TruongHoc": null,
+		"DoiTac": null,
+		"makichhoat": null,
+		"sessionLogin": "637360336007842419",
+		"Id": 30,
+		"CreatedById": 0,
+		"CreatedDate": null,
+		"UpdatedById": 0,
+		"UpdatedDate": null
+	},
+	"listdatalock": ["cate_en_1=0", "cate_en_2=0", "cate_en_3=0", "cate_en_4=0", "cate_en_5=0", "cate_en_6=0", "cate_en_364=0", "cate_en_447=0", "cate_en_468=0", "cate_en_469=0", "cate_en_573=0", "cate_en_614=0", "cate_en_615=0", "cate_phonic_1=0", "cate_phonic_28=0", "cate_mat_10=0", "cate_mat_11=0", "cate_mat_12=0", "cate_mat_13=0", "cate_mat_20=0", "cate_mat_23=0", "cate_lt_726=0", "cate_lt_727=0", "cate_lt_735=0", "cate_lt_744=0", "cate_lt_757=0", "cate_lt_758=0"]
+};
+
+$done({body: JSON.stringify(obj)});
+
+
+// Descriptions
