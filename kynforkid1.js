fa43bf1ebeb718a447461a538b20e759e267dcda
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-09-18 23:44:54 +0700

    Update kynforkid1.js

--- kynforkid1.js
+++ kynforkid1.js
@@ -4,11 +4,11 @@ obj= {
 	"data": {
 		"access_token": "13060a78897ee79edacf817a0623bcc5a590e2b22d1e676490dd2dee8270fc23/v7xYBA6zJJz2WFaH4OWbfdFa24GD4qm2njavf260-NhHxdetLqzkkzXhlVh07vuv/MzY4MzAz",
 		"signature": "2204cc495c25ade4875025eaaac37edeefb8d5ad190ccb23b547ae14552e9dee",
-		"user_id": 368303,
+		"user_id": 999999,
 		"name": "Quang Anh",
 		"avatar": "/default/user/kid/avatar-01.png",
-		"email": "quanganh@kynaforkids.vn",
-		"phone_number": "0966066008",
+		"email": "abcdef@gmail.com",
+		"phone_number": "0901234567",
 		"is_confirmed": true,
 		"params": {
 			"notification_topic": "app_notification_topic_staging"
@@ -21,7 +21,7 @@ obj= {
 			"cdn_min_version": "1.2.4"
 		},
 		"children": [{
-			"id": 368409,
+			"id": 111111,
 			"name": "Jun",
 			"avatar": "/default/user/kid/avatar-01.png",
 			"birthday": 1441083313,
@@ -30,7 +30,7 @@ obj= {
 			"expired_time_acc": -1,
 			"has_booking": false
 		}, {
-			"id": 368447,
+			"id": 222222,
 			"name": "Đông",
 			"avatar": "/default/user/kid/avatar-01.png",
 			"birthday": 1343798920,
