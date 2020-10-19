08387e520f5c168bf9230a4a4cced54d2c511238
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-10-19 13:02:29 +0700

    Update LittleFox.js

--- LittleFox.js
+++ LittleFox.js
@@ -1,8 +1,15 @@
 var obj = JSON.parse($response.body);
-obj.data.contents.service_info.service="F";
-obj.data.contents.service_info.story="F";
-obj.data.contents.service_info.original_text="F";
-obj.data.contents.service_info.quiz="F";
-obj.data.contents.service_info.ebook="F";
-obj.data.contents.service_info.vocabulary="F";
+obj.data.contents.service_info=
+{
+				"service": "F",
+				"story": "F",
+				"original_text": "F",
+				"vocabulary": "N",
+				"quiz": "F",
+				"sentence": "N",
+				"crossword": "N",
+				"translate": "N",
+				"starwords": "N",
+				"ebook": "F"
+			};
 $done({body: JSON.stringify(obj)});
