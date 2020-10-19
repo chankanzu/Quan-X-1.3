f5f8e3eef3d29f786f8472b87baad51ff69107ff
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2020-10-19 12:51:08 +0700

    Update LittleFox.js

--- LittleFox.js
+++ LittleFox.js
@@ -1,32 +1,8 @@
 var obj = JSON.parse($response.body);
-obj= {
-	"data": {
-		"current_user_id": "U201811112340560107",
-		"login_id": "svcntnk42a1@gmail.com",
-		"country_code": "US",
-		"language_code": "en",
-		"www_url": "https:\/\/www.littlefox.com\/en",
-		"mobile_url": "https:\/\/m.littlefox.com\/en",
-		"users": [{
-			"id": "U201811112340560107",
-			"type": "S",
-			"name": " ",
-			"nickname": "Khanh Du",
-			"gender": "M",
-			"avatar_image_url": "https:\/\/img.littlefox.co.kr\/static\/layout\/global\/img\/contents\/default_badge.png",
-			"is_custom_avatar": "N"
-		}, {
-			"id": "U201811120027560110",
-			"type": "S",
-			"name": "Jun",
-			"nickname": "QuangAnh",
-			"birth_year": 2015,
-			"gender": "M",
-			"avatar_image_url": "https:\/\/img.littlefox.co.kr\/static\/layout\/global\/img\/contents\/default_badge.png",
-			"is_custom_avatar": "N"
-		}]
-	},
-	"status": 200
-};
+obj.data.contents.service_info.service="F";
+obj.data.contents.service_info.story="F";
+obj.data.contents.service_info.original_text="F";
+obj.data.contents.service_info.quiz="F";
+obj.data.contents.service_info.ebook="F";
+obj.data.contents.service_info.vocabulary="F";
 $done({body: JSON.stringify(obj)});
-//
