97f2c924841c7f9ad794d68ce52bbd1e81c10040
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2021-08-18 07:57:52 +0700

    Update 4english.vip.js

--- 4english.vip.js
+++ 4english.vip.js
@@ -1,15 +1,26 @@
 var obj = JSON.parse($response.body);
 
 obj= {
-	"code": 200,
+	"code": 0,
 	"data": {
-		"userId": "2fwiWLaVkvg3IxakMqhn6AH8DkA3",
-		"shortUserId": "951342",
-		"email": "svcntnk42a1@gmail.com",
-		"displayName": "Khánh Dư Trần",
+		"userId": "inrTPiukigOedHWlS0j4d1Rygse2",
+		"shortUserId": "732315",
+		"email": "uservip@4english.com",
+		"displayName": "Teddy LOL",
 		"providerData": "Firebase",
-		"expiredTime": "4086356907000",
-		"showOxford": true
+		"expiredTime": "10244015999999",
+		"showOxford": false,
+		"showOxfordWeb": false,
+		"win": 4,
+		"lose": 3,
+		"setting": {
+			"level": "intermediate",
+			"goal": "marketing",
+			"appVersion": 9012,
+			"nativeLanguage": "vi",
+			"topics": ["society", "education", "politic"]
+		},
+		"createAt": 1603960581817
 	}
 };
 
