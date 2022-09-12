6fe5e19a508a7ff131d53f80362917c9180ec532
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2022-09-12 23:31:19 +0700

    Update Monkeycombo.js

--- Monkeycombo.js
+++ Monkeycombo.js
@@ -2,9 +2,9 @@ var obj = JSON.parse($response.body);
 
 obj.data.purchased=
 {
-			"pay_inapp": false,
-			"time_purchased": false,
-			"time_activated": 1662998952,
+			"pay_inapp": true,
+			"time_purchased": 0,
+			"time_activated": 0,
 			"time_expire": 0,
 			"product_list": [],
 			"is_active": true,
