ad88d72a9d719ceb683a110fcd7790c9f9e6a758
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2022-10-02 21:46:09 +0700

    Update busuu.vip.js

--- busuu.vip.js
+++ busuu.vip.js
@@ -1,3 +1,15 @@
 var obj = JSON.parse($response.body);
-obj.data.is_premium= true;
+;obj.data.is_premium= true;
+
+obj.data.premium_data.current_plan.active= true;
+obj.data.premium_data.subscription_status="active";
+obj.data.premium_data.subscriptionType= "1y";
+obj.data.premium_data.free_trial.free_trial= true;
+obj.data.premium_data.expiration= 1685289207;
+obj.data.premium_data.next_charge= 1685289207;
+
 $done({body: JSON.stringify(obj)});
+
+
+
+
