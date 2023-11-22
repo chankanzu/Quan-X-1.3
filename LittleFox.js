237c3905540d2cf5f6e91d06a54b71c786cef0b2
Trần Khánh Dư <42104328+chankanzu@users.noreply.github.com>
2023-11-22 22:50:27 +0700

    Update LittleFox.js

--- LittleFox.js
+++ LittleFox.js
@@ -1,7 +1,3 @@
-// Phân tích cú pháp của phản hồi
-var responseBody = $response.body;
-var parsedResponse = JSON.parse(responseBody);
-
 // Định nghĩa đối tượng service_info với các thuộc tính cụ thể
 var serviceInfo = {
     service: "F",
@@ -16,9 +12,24 @@ var serviceInfo = {
     ebook: "F"
 };
 
-// Gán đối tượng service_info vào vị trí thích hợp trong đối tượng đã được phân tích cú pháp
-parsedResponse.data.contents.service_info = serviceInfo;
+// Tạo đối tượng user_service_info với các thuộc tính tương tự như service_info, nhưng có giá trị khác
+var userServiceInfo = {
+    service: "F",
+    story: "Y",
+    original_text: "Y",
+    vocabulary: "Y",
+    quiz: "Y",
+    sentence: "Y",
+    crossword: "Y",
+    translate: "Y",
+    starwords: "Y",
+    ebook: "Y"
+};
 
-// Chuyển đối tượng đã sửa đổi thành chuỗi JSON và hoàn tất hành động
+// Gán các đối tượng vào các vị trí thích hợp trong đối tượng đã được phân tích cú pháp
+parsedResponse.data.contents.service_info = serviceInfo;
+parsedResponse.data.contents.user_service_info = userServiceInfo;
+
+// Chuyển đổi đối tượng đã sửa đổi thành chuỗi JSON và hoàn tất hành động
 var modifiedBody = JSON.stringify(parsedResponse);
 $done({ body: modifiedBody });
